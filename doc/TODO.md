a5c8fa211c4c2cb33f7821d03a5cae63d78d3c1a
Pripyat Automations <joseph@pbx.voip.pripyat.lan>
2025-01-02 20:06:38 +0000

    Cleanup

--- doc/TODO.md
+++ doc/TODO.md
@@ -2,7 +2,8 @@
 - fix cisco-login.pl
 - Implement img_on and img_off types (exclusive of img)
 - Make script to validate the yaml using yq for syntax
-  - Confirm it has Position attrib on softkeys
+  - Confirm it has Position attrib on softkeys and throw error instead of
+    emitting broken XML
 - Auth:
     * Store device name, IP when sent
     * Create a cookie, stored in database which we
--- doc/cisco.sqlite3.sql
+++ doc/cisco.sqlite3.sql
@@ -1,5 +1,7 @@
 drop table if exists user_sessions;
 drop table if exists users;
+drop table if exists groups;
+drop table if exists group_members;
 
 CREATE TABLE user_sessions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,10 +21,49 @@ CREATE TABLE users (
     name TEXT NOT NULL,
     pin TEXT NOT NULL
 );
+
+create table groups (
+   gid INTEGER PRIMARY KEY AUTOINCREMENT,
+   grp_name TEXT NOT NULL
+);
+
+--- One entry per user/group pairing.
+create table group_members (
+   gmid INTEGER PRIMARY KEY AUTOINCREMENT,
+   gid INTEGER NOT NULL,
+   uid INTEGER NOT NULL
+);
+
 --- XXX: ToDo - Create a relationship between users.id and user_sessions.uid
+--- Also for group_members.(gid|uid) to respective tables
 CREATE UNIQUE INDEX idx_ip_dev_name ON user_sessions(ip_address, dev_name);
 CREATE UNIQUE INDEX idx_token ON user_sessions(token);
 
 --- Add users
 --- Example:
---- INSERT INTO users (name, pin) VALUES ('eel', '4004');
+INSERT INTO users (id, name, pin) VALUES (0, 'eel', '4004');
+INSERT INTO users (name, pin) VALUES ('joe', '1551');
+--- And some groups
+INSERT INTO groups (gid, grp_name) VALUES (0, 'users');
+INSERT INTO groups (gid, grp_name) VALUES (1, 'admins');
+INSERT INTO groups (gid, grp_name) VALUES (2, 'radio');
+INSERT INTO groups (gid, grp_name) VALUES (3, 'radio-admins');
+
+--- Add all users to the 'users' group
+INSERT INTO group_members (gid, uid)
+SELECT g.gid, u.id
+FROM users u
+JOIN groups g ON g.grp_name = 'users';
+
+--- Now make the first user an admin, if none exists
+INSERT INTO group_members (gid, uid)
+SELECT g.gid, u.id
+FROM users u
+JOIN groups g ON g.grp_name = 'admins'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM group_members gm
+    WHERE gm.gid = g.gid
+)
+ORDER BY u.id ASC
+LIMIT 1;
