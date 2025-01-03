2dae8359ffa4e2255398e4545db0b95a976e548d
root <root@pbx.voip.pripyat.lan>
2025-01-03 06:22:23 +0000

    added dnsmasq.conf and a demo start script

--- bin/start-demo.sh
+++ bin/start-demo.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo $PWD/bin/start-fastcgi-perl-wrapper.pl start
+sudo $PWD/bin/start-fastcgi-perl-wrapper.pl restart
 echo "* Starting builtin webserver (nginx)"
 nginx -c $PWD/etc/nginx/nginx.conf
 
 # XXX: Check configuration for tftp enable
-#echo "* Starting dnsmasq tftp server"
-#dnsmasq -C $PWD/etc/dnsmasq.conf
+echo "* Starting dnsmasq tftp server"
+dnsmasq -C $PWD/etc/dnsmasq.conf
